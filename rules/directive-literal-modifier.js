e1bcab7eb642276fbba02e1f626b6a648fd004c9
Chris Fritz <chrisvfritz@gmail.com>
2016-09-08 23:53:59 -0400

    catch directive literal modifier

---
+++ rules/directive-literal-modifier.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var chalk = require('chalk')
+
+module.exports = {
+  pattern: /\b(v-[\w-]+)([\w-\.:]*?)\.literal([\w-\.]*?)="(.+?)"/,
+  warning: function (match, name, preLiteralStuff, postLiteralStuff, value) {
+    return {
+      reason: 'Directives have been vastly simplified and no longer include the literal modifier',
+      fix: (
+        'Replace ' + chalk.red(match) + ' with ' +
+        chalk.green(name + preLiteralStuff + postLiteralStuff + '="\'' + value + '\'"')
+      ),
+      docsHash: 'dispatch-and-broadcast'
+    }
+  }
+}
