dd2b97a5f8cfb16380e0a134fbd5e00bdfc0b77c
Chris Fritz <chrisvfritz@gmail.com>
2016-09-08 22:50:29 -0400

    add rule for dispatch and broadcast

---
+++ rules/dispatch-and-broadcast.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var chalk = require('chalk')
+
+module.exports = {
+  pattern: /(\w+)\.\$(?:dispatch|broadcast)\s*?\(.+?\)/,
+  warning: function (match) {
+    return {
+      reason: '$dispatch and $broadcast have been deprecated because the pattern doesn\'t scale well',
+      fix: 'Replace ' + chalk.red(match) + ' to use a global event bus or vuex (see link below for implementation details)',
+      docsHash: 'dispatch-and-broadcast'
+    }
+  }
+}
