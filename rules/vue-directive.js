51de13e3a06dc46b47151914334827b8b97646d1
Chris Fritz <chrisvfritz@gmail.com>
2016-09-09 00:10:59 -0400

    add vue directive warning

---
+++ rules/vue-directive.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var chalk = require('chalk')
+
+module.exports = {
+  pattern: /Vue\.directive\(\s*?(['"`].+?['"`])/,
+  warning: function (match, name) {
+    return {
+      reason: 'Directives have been vastly reduced in scope and components are preferred in most use cases',
+      fix: (
+        'Check ' + chalk.red('Vue.directive(' + name + ')') + ' to make sure its syntax has been updated and for anything beyond simple DOM manipulations, refactor to a component'
+      ),
+      docsHash: 'Directives'
+    }
+  }
+}
