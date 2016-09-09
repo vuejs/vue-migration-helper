6746ad6e3a922ffc0ccd106acd1ec43580c8f373
Chris Fritz <chrisvfritz@gmail.com>
2016-09-08 23:16:22 -0400

    catch implicit  from v-for

---
+++ rules/v-for-implicit-index-and-key.js
@@ -0,0 +1,20 @@
+'use strict'
+
+var chalk = require('chalk')
+
+module.exports = {
+  pattern: /\$(index|key)/,
+  warning: function (match) {
+    const exampleVFor = match === '$index'
+      ? 'v-for="(item, index) in items"'
+      : 'v-for="(value, key) in object"'
+    return {
+      reason: match + ' has been deprecated to avoid implicitly defined (i.e. "magic") variables',
+      fix: (
+        'Rename ' + chalk.red(match) + ' to ' +
+        chalk.green(match.slice(1)) + ' and explicity declare it (e.g. ' + exampleVFor + ')'
+      ),
+      docsHash: 'v-for-Implicit-index-and-key'
+    }
+  }
+}
