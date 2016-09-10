6519a67f661133b192e0251a2874c30aa87713a8
Chris Fritz <chrisvfritz@gmail.com>
2016-09-10 02:18:38 -0400

    catch  assignment

---
+++ rules/data-assignment.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var chalk = require('chalk')
+
+module.exports = {
+  pattern: /[\w+\.]+?\.\$data\s*?=[^\n]+/,
+  warning: function (match) {
+    return {
+      reason: 'Replacing $data is now longer allowed, as it often has unintended side effects',
+      fix: (
+        'Instead of replacing $data with ' + chalk.red(match) +
+        ', update individual properties or scope all the properties you want to update under a new object property, then replace that object'
+      ),
+      docsHash: 'data-Assignment'
+    }
+  }
+}
