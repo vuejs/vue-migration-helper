f714f100193d88537f1ef6efa1f4f6f5740465ed
Chris Fritz <chrisvfritz@gmail.com>
2016-09-10 02:36:54 -0400

    add count to warning reporter

--- helpers/report-warning.js
+++ helpers/report-warning.js
@@ -2,11 +2,16 @@
 
 var chalk = require('chalk')
 
+var warningCount = 0
 module.exports = function (fileData, warning) {
   if (process.env.NODE_ENV === 'test') return
 
+  warningCount++
+
   console.log()
-  console.log(chalk.yellow(warning.fix))
+  console.log(chalk.yellow(
+    warningCount + '. ' + warning.fix
+  ))
   console.log(chalk.blue('  Line ' + fileData.lineNum + ': ' + fileData.file))
   console.log(chalk.cyan.dim('  Reason: ' + warning.reason))
   console.log(chalk.cyan.dim(
