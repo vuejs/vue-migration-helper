1f4efaf227fbe6118f276423f5b80977f94667f9
Chris Fritz <chrisvfritz@gmail.com>
2016-09-10 01:15:35 -0400

    catch v-show with v-else

---
+++ rules/v-show-with-v-else.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var chalk = require('chalk')
+
+module.exports = {
+  pattern: /<[^>]+\b(?:v-else[^>]+?\b(v-show=["'][^\[\.]+["'])|(v-show=["'][^\[\.]+["'])[^>]+?v-else)[^>]*?>/,
+  warning: function (match, vShowAfter, vShowBefore) {
+    return {
+      reason: 'When used with v-show, a negation expression must be used instead of v-else, e.g. v-if="!foo"',
+      fix: (
+        'Replace ' + chalk.red('v-else') + ' with a ' +
+        chalk.green('v-if') + ' negation expression'
+      ),
+      docsHash: 'v-show-with-v-else'
+    }
+  }
+}
---
+++ rules/v-show-with-v-else.spec.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const check = createRuleChecker('v-show-with-v-else')
+
+describe('Rule: v-show-with-v-else', () => {
+  it('does not match an empty line', () => {
+    const warning = check('')
+    expect(warning).toBe(null)
+  })
+
+  it('does not match a simple v-else', () => {
+    const warning = check(`
+      <p v-else></p>
+    `)
+    expect(warning).toBe(null)
+  })
+
+  it('matches a simple v-show with v-else, with v-else first', () => {
+    const warning = check(`
+      <p v-else v-show="foo"></p>
+    `)
+    expect(warning).toBeTruthy()
+    expect(warning.fix).toBe('Replace v-else with a v-if negation expression')
+  })
+
+  it('matches a simple v-show with v-else, with v-show first', () => {
+    const warning = check(`
+      <p v-show="foo" v-else></p>
+    `)
+    expect(warning).toBeTruthy()
+    expect(warning.fix).toBe('Replace v-else with a v-if negation expression')
+  })
+})
