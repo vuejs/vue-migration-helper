7450691d0570c56b2739c07c7b849423c91a02c8
Chris Fritz <chrisvfritz@gmail.com>
2016-09-10 18:22:09 -0400

    fix formatting of debouce-param instructions

--- rules/debounce-param.js
+++ rules/debounce-param.js
@@ -8,10 +8,7 @@ module.exports = {
     return {
       reason: 'debounce was quite limited and reinvented a utility function better maintained in libraries such as lodash and underscore',
       fix: (
-        'Delete ' + chalk.red(match) + ' and ' +
-        chalk.green(
-          'wrap the expensive operation(s) you want to throttle with lodash\'s debounce or throttle functions (see the link below for an example)'
-        )
+        'Delete ' + chalk.red(match) + ' and wrap the expensive operation(s) you want to throttle with lodash\'s debounce or throttle functions (see the link below for an example)'
       ),
       docsHash: 'debounce-param'
     }
