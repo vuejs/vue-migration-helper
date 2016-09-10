8e820953202002b576fe9546856bc49786afa58e
Chris Fritz <chrisvfritz@gmail.com>
2016-09-10 01:26:55 -0400

    match one-time-bindings

---
+++ rules/one-time-binding.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var chalk = require('chalk')
+
+module.exports = {
+  pattern: /\{\{\s*?\*(.+?)\}\}/,
+  warning: function (match, binding) {
+    return {
+      reason: 'One-way bindings have been deprecated in favor of a new v-once directive',
+      fix: (
+        'Replace ' + chalk.red(match) + ' with ' +
+        chalk.green('v-once="' + binding.trim() + '"') +
+        ' on a containing element'
+      ),
+      docsHash: 'One-Time-Bindings'
+    }
+  }
+}
