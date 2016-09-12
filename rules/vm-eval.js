5625d8662e2f8d042cb07059f4ea57f4f7a6586b
Chris Fritz <chrisvfritz@gmail.com>
2016-09-11 21:48:06 -0400

    catch vm.

---
+++ rules/vm-eval.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var chalk = require('chalk')
+
+module.exports = {
+  pattern: /(this|vm|self)\.\$eval\s*?\(\s*?["'`](.+?)["'`]\s*?\)/,
+  warning: function (match, vm, property) {
+    return {
+      reason: 'vm.$eval has been deprecated, as it has no real use',
+      fix: (
+        'Replace ' + chalk.red(match) + ' with a solution using normal JavaScript'
+      ),
+      docsHash: 'vm-set'
+    }
+  }
+}
