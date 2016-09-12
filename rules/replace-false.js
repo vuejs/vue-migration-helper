c7e09f6e5d31433f7dbb78e9d4b7ded3b691ec91
Chris Fritz <chrisvfritz@gmail.com>
2016-09-11 21:02:44 -0400

    catch replace: false

---
+++ rules/replace-false.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var chalk = require('chalk')
+
+module.exports = {
+  pattern: /\breplace\s*?:\s*?false\b/,
+  warning: function (match) {
+    return {
+      reason: 'The replace option has been deprecated, as it provides too little convenience',
+      fix: (
+        'Delete the ' + chalk.red(match) + ' option and instead wrap your root component with an element similar to the one you\'re replacing, e.g. ' +
+        chalk.green('el: \'#app\'') +
+        ' with ' +
+        chalk.green('template: \'<div id="app"> ... </div>\'')
+      ),
+      docsHash: 'replace-false'
+    }
+  }
+}
