0aa5205ca972229db2fa9889555c6c1b98e6ea20
Chris Fritz <chrisvfritz@gmail.com>
2016-09-11 22:52:10 -0400

    catch vm dom methods: , , ,

---
+++ rules/vm-dom-methods.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var chalk = require('chalk')
+
+module.exports = {
+  pattern: /(this|vm|self)\.\$(appendTo|before|after|remove)\s*?\(\s*?(.+?)\s*?\)/,
+  warning: function (match, vm, method, elementOrSelector) {
+    const element = /['"`]/.test(elementOrSelector)
+      ? 'document.querySelector(' + elementOrSelector + ')'
+      : elementOrSelector
+    const replacement = {
+      appendTo: element + '.appendChild(' + vm + '.$el' + ')',
+      before: element + '.parentNode.insertBefore(' + vm + '.$el, ' + element + ')',
+      after: element + '.parentNode.insertBefore(' + vm + '.$el, ' + element + '.nextSibling)',
+      remove: vm + '.$el.remove()'
+    }[method]
+    return {
+      reason: 'vm.$' + method + ' has been deprecated in favor of simply using the native DOM API on vm.$el',
+      fix: (
+        'Replace ' + chalk.red(match) + ' with ' +
+        chalk.green(replacement)
+      ),
+      docsHash: 'vm-eval'
+    }
+  }
+}
