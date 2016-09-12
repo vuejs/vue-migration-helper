d2acab49bcac1e6de87397d74404dc71f89e786e
Chris Fritz <chrisvfritz@gmail.com>
2016-09-11 22:13:25 -0400

    catch vm.

---
+++ rules/vm-log.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var chalk = require('chalk')
+
+module.exports = {
+  pattern: /(this|vm|self)\.\$log\s*?\(\s*?["'`](.+?)["'`]\s*?\)/,
+  warning: function (match, vm, property) {
+    return {
+      reason: 'vm.$log has been deprecated, because the Vue Devtools offer a better debugging experience',
+      fix: (
+        'Delete ' + chalk.red(match) + ' and install the Vue Devtools instead: ' + chalk.underline.green('https://github.com/vuejs/vue-devtools')
+      ),
+      docsHash: 'vm-log'
+    }
+  }
+}
---
+++ rules/vm-log.spec.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const check = createRuleChecker('vm-log')
+
+describe('Rule: vm-log', () => {
+  it('does not match an empty line', () => {
+    const warning = check('')
+    expect(warning).toBe(null)
+  })
+
+  it('matches this.$log(\'{{msg}} world!\')', () => {
+    const warning = check(`
+      this.$log('{{msg}} world!')
+    `)
+    expect(warning).toBeTruthy()
+    expect(warning.fix).toBe('Delete this.$log(\'{{msg}} world!\') and install the Vue Devtools instead: https://github.com/vuejs/vue-devtools')
+  })
+
+  it('matches vm.$log(\'{{msg}} world!\')', () => {
+    const warning = check(`
+      vm.$log('{{msg}} world!')
+    `)
+    expect(warning).toBeTruthy()
+    expect(warning.fix).toBe('Delete vm.$log(\'{{msg}} world!\') and install the Vue Devtools instead: https://github.com/vuejs/vue-devtools')
+  })
+
+  it('matches self.$log(\'{{msg}} world!\')', () => {
+    const warning = check(`
+      self.$log('{{msg}} world!')
+    `)
+    expect(warning).toBeTruthy()
+    expect(warning.fix).toBe('Delete self.$log(\'{{msg}} world!\') and install the Vue Devtools instead: https://github.com/vuejs/vue-devtools')
+  })
+})
