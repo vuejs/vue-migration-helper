e04e3bc5ec415ba3c37f67dbef0a7a1885fd08c3
Chris Fritz <chrisvfritz@gmail.com>
2016-09-11 21:25:49 -0400

    catch twoWay: true

---
+++ rules/twoway-true.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var chalk = require('chalk')
+
+module.exports = {
+  pattern: /\btwoWay\s*?:\s*?true\b/,
+  warning: function (match) {
+    return {
+      reason: 'Two-way prop binding has been deprecated, in favor of more explicit event-driven communication between parent and child',
+      fix: (
+        'Delete ' + chalk.red(match) + ', then $emit an event from the child component to trigger an update to the prop in the parent'
+      ),
+      docsHash: 'twoWay-true'
+    }
+  }
+}
