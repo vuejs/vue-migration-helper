6a8a0f0d4ee62ce9255c5b209beef106356945ec
Alex Kyriakidis <hootlex@icloud.com>
2016-09-23 00:44:18 +0300

    apply repos style

--- helpers/track-stats.js
+++ helpers/track-stats.js
@@ -1,30 +1,28 @@
 'use strict'
 var chalk = require('chalk')
 
-module.exports = function(proms) {
+module.exports = function (proms) {
+  Promise.all(proms)
+    .then(function (data) {
+      var allFiles = proms.length
+      var filesWithErr = data.filter(function (x) {
+        return x > 0
+      }).length
 
-    Promise.all(proms)
-        .then(function(data) {
+      var errCount = data.reduce(function (total, x) {
+        return total + x
+      })
 
-            var filesWithErr = data.filter(function(x) {
-                return x > 0
-            }).length
-
-            var errCount = data.reduce(function(total, x) {
-                return total + x
-            });
-
-            var allFiles = proms.length;
-
-            if (filesWithErr === 0) {
-                console.log(chalk.green(
-                    '✓ Project\'s files don\'t use deprecated patterns.'
-                ))
-            } else {
-                console.log(chalk.white.bgRed(
-                    ' ✖ ' + errCount + ' deprecated patterns detected in ' + filesWithErr + ' out of ' + allFiles + ' project\'s files. '
-                ))
-            }
-        })
-
+      // Display stats
+      console.log()
+      if (filesWithErr === 0) {
+        console.log(chalk.green(
+          '✓ Project\'s files don\'t use deprecated patterns.'
+        ))
+      } else {
+        console.log(chalk.white.bgRed(
+          ' ✖ ' + errCount + ' deprecated patterns detected in ' + filesWithErr + ' out of ' + allFiles + ' project\'s files. '
+        ))
+      }
+    })
 }
--- index.js
+++ index.js
@@ -13,37 +13,31 @@ var args = process.argv.slice(2)
 var folders = args.length ? args.length === 1 ? args + '/**/*' : '{' + args.join(',') + '}/**/*' : '**/*'
 
 glob(folders, {
-    nodir: true,
-    ignore: [
-        '**/.git/**/*',
-        '**/node_modules/**/*',
-        '**/*.+(jpe?g|gif|png|svg|woff2?|ttf|otf|eot|log|zip|map)'
-    ]
-}, function(error, files) {
-    if (error) throw error
-    var proms = files.map(function(file) {
-
-        return new Promise(function(resolve, reject) {
-
-            var lineNum = 0
-            var errorsCount = 0;
-            fs.createReadStream(file)
-                .pipe(split())
-                .on('data', function(line) {
-
-                    errorsCount += checkForDeprecations({
-                        line: line,
-                        lineNum: ++lineNum,
-                        file: file
-                    }) ? 1 : 0
-                })
-                .on('end', function() {
-                    resolve(errorsCount)
-                })
+  nodir: true,
+  ignore: [
+    '**/.git/**/*',
+    '**/node_modules/**/*',
+    '**/*.+(jpe?g|gif|png|svg|woff2?|ttf|otf|eot|log|zip|map)'
+  ]
+}, function (error, files) {
+  if (error) throw error
+  var proms = files.map(function (file) {
+    return new Promise(function (resolve, reject) {
+      var lineNum = 0
+      var errorsCount = 0
+      fs.createReadStream(file)
+        .pipe(split())
+        .on('data', function (line) {
+          errorsCount += checkForDeprecations({
+            line: line,
+            lineNum: ++lineNum,
+            file: file
+          }) ? 1 : 0
         })
-
+        .on('end', function () {
+          resolve(errorsCount)
+        })
     })
-
-    trackStats(proms)
-
+  })
+  trackStats(proms)
 })
